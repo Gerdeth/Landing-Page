02a9aa1750493e18e499c65a2874110307c0bd95
Gerdeth <gerdethwickham@gmail.com>
2020-09-30 15:53:21 -0400

    first commit

---
+++ home.js
@@ -0,0 +1,26 @@
+
+
+
+
+<header id= "header">
+  <h6 className="dropdown-header">Dr. Gerdean Wickham</h6>
+  <img id="header-img" src="https://n-o-v-a.com/wp-content/uploads/2015/09/Physical-Therapy2.png" alt="Physical Therapy "></img>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light" id ="nav-bar">
+  <a className="navbar-brand" href="#">Home</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+      
+      <li className="nav-item">
+        <a className="nav-link" href="#">What we do</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Pricing</a>
+      </li>
+    
+    </ul>
+  </div>
+</nav>
+</header>
